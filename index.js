3a547267ad338619325cd270a7c925bb7b47992b
samuel1201 <samuel1201@gmail.com>
2023-10-15 18:12:45 +0800

    基本設定

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-<h1>Hello!</h1>
+fetch('page/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    });
+
+fetch('page/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    });
+
+fetch('page/home.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('content').innerHTML = data;
+    });
