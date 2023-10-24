07b3eaab0ff05bfb8c6e182cbc48c24db6397e00
Yatayork <103381337+yatayork@users.noreply.github.com>
2023-10-24 13:06:23 +0800

    Update backstageView.js

--- frontend/static/js/views/backstageView.js
+++ frontend/static/js/views/backstageView.js
@@ -8,68 +8,60 @@ export default class extends AbstractView {
     async getHtml() {
         return `
         <div class="backstage padLR-32px">
-        <div class="bottom_border">
-            <h3 class="h3type">帳號管理</h3>
-        </div>
-        <div class="buildings flex bottom_border">
-            <a href="#" class="building_button">
-                A棟
-            </a>
-            <a href="#" class="building_button">
-                B棟
-            </a>
-            <a href="#" class="building_button">
-                C棟
-            </a>
-            <a href="#" class="building_button">
-                D棟
-            </a>
-        </div>
-        <div class="permissionControl">
-            <table>
-                <colgroup span="4"></colgroup>
-                <tr>
-                    <th></th>
-                    <th>戶號</th>
-                    <th>管理權限開啟</th>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="checkbox" class="check" id="" />
-                    </td>
-                    <td>A01-01</td>
-                    <td class="relative">
-                        <div class="center">
-                            <input type="checkbox" name="permissionControl" class="switch" />
+                        <div class="bottom_border">
+                            <h3 class="h3type">帳號管理</h3>
                         </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="checkbox" class="check" id="" />
-                    </td>
-                    <td>A01-02</td>
-                    <td class="relative">
-                        <div class="center">
-                            <input type="checkbox" name="permissionControl" class="switch" />
+                        <div class="buildings flex bottom_border">
+                            <a href="#" class="building_button">A棟</a>
+                            <a href="#" class="building_button">B棟</a>
+                            <a href="#" class="building_button">C棟</a>
+                            <a href="#" class="building_button">D棟</a>
+
+                            <select id="building_select" class="building_select">
+                                <option value="A棟">A棟</option>
+                                <option value="B棟">B棟</option>
+                                <option value="C棟">C棟</option>
+                                <option value="D棟">D棟</option>
+                            </select>
                         </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="checkbox" class="check" id="" />
-                    </td>
-                    <td>A01-03</td>
-                    <td class="relative">
-                        <div class="center">
-                            <input type="checkbox" name="permissionControl" class="switch" />
+                        <div class="permissionControl">
+                            <table>
+                                <tr>
+                                    <th></th>
+                                    <th>戶號</th>
+                                    <th>管理權限開啟</th>
+                                </tr>
+                                <tr>
+                                    <td><input type="checkbox" class="check" id="" /></td>
+                                    <td>A01-01</td>
+                                    <td class="relative">
+                                        <div class="center">
+                                            <input type="checkbox" name="permissionControl" class="switch" />
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td><input type="checkbox" class="check" id="" /></td>
+                                    <td>A01-02</td>
+                                    <td class="relative">
+                                        <div class="center">
+                                            <input type="checkbox" name="permissionControl" class="switch" />
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td><input type="checkbox" class="check" id="" /></td>
+                                    <td>A01-03</td>
+                                    <td class="relative">
+                                        <div class="center">
+                                            <input type="checkbox" name="permissionControl" class="switch" />
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
                         </div>
-                    </td>
-                </tr>
-            </table>
-        </div>
-        <br />
-    </div>
+                        <br />
+                    </div>
         `;
     }
 }
